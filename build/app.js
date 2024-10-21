febd7d45550b368e548537c69e76b634711a17d5
pineappletaco <411630782@o365.tku.edu.tw>
2024-10-21 20:45:35 +0800

    add something

--- build/app.js
+++ build/app.js
@@ -1,19 +1,17 @@
-import { Rational } from "./class/Rational";
-
-const main = (): void => {
-
-    const R1: Rational = new Rational(6, 4);
-    const R2: Rational = new Rational(6, 4).normalize();
-    const R3: Rational = new Rational(12, 4);
-    const R4: Rational = Rational._parseRational(["1", "3"], ["4", "1"]);
-    const R5: Rational = Rational.parseRational("43/23");
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rational_1 = require("./class/Rational");
+const main = () => {
+    const R1 = new Rational_1.Rational(6, 4);
+    const R2 = new Rational_1.Rational(6, 4).normalize();
+    const R3 = new Rational_1.Rational(12, 4);
+    const R4 = Rational_1.Rational._parseRational(["1", "3"], ["4", "1"]);
+    const R5 = Rational_1.Rational.parseRational("43/23");
     // 打印物件
     console.log(`R1 : ${R1.toString()}`);
     console.log(`R2 : ${R2.toString()}`);
     console.log(`R3 : ${R3.toString()}`);
     console.log();
-
     // 檢測相等
     console.log(`R1.equals(R2) : ${R1.equals(R2)}`);
     // 檢測整數
@@ -21,7 +19,6 @@ const main = (): void => {
     // 檢測有無小數
     console.log(`R1.isDecimal() : ${R1.isDecimal()}`);
     console.log();
-
     // 檢測相等
     console.log(`R1.equals(R3) : ${R1.equals(R3)}`);
     // 檢測整數
@@ -29,17 +26,12 @@ const main = (): void => {
     // 檢測有無小數
     console.log(`R3.isDecimal() : ${R3.isDecimal()}`);
     console.log();
-
     // 打印物件
     console.log(`R4 : ${R4.toString()}`);
     console.log(`R5 : ${R5.toString()}`);
-
-
 };
-
 main();
-
-/* 
+/*
 result:
 
 R1 : 6/4
